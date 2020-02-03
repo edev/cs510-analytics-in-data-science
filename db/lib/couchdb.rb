af59a29015993f388422e471e38127496d2d22d3
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2020-02-02 17:41:50 -0800

    Refactored CouchDB::uri() for ease of use.
    Now the caller simply passses the portion of the path that comes
    after the database and gets a URI object back, ready to go. Now
    supports queries and fragments as well.

--- db/lib/couchdb.rb
+++ db/lib/couchdb.rb
@@ -2,40 +2,43 @@ require 'net/http'
 require 'json'
 
 module CouchDB
-    HOSTNAME = 'localhost'
-    PORT = 5984
-    DB = '/bounty'
-    JSON_HEADER = { 'Content-Type': 'application/json' }
+  HOSTNAME = 'localhost'
+  PORT = 5984
+  DB = 'bounty'
+  JSON_HEADER = { 'Content-Type': 'application/json' }
 
-    ##
-    # Helper method to get a URI for a given path.
-    def self.uri(path)
-        URI::HTTP.build(
-          host: HOSTNAME,
-          port: PORT,
-          path: path
-        )
+  ##
+  # Helper method to generate a URI object for a CouchDB path.
+  #
+  # The helper will auto-fill everything from http:// through the database name and trailing slash. To get the URI of
+  # the database itself, pass path="". For any subpath, provide just the subpath portion of the URI. For instance,
+  # to access the CouchDB API path /{db}/_design/{design-doc} for a design document named foo, pass path='_design/foo'.
+  #
+  # Any queries or fragments, e.g. "?foo=bar#baz", should be passed in path as well.
+  #
+  # Returns a URI object appropriate for a CouchDB request.
+  def self.uri(path)
+    URI("http://#{HOSTNAME}:#{PORT}/#{DB}/#{path}")
+  end
+
+  ##
+  # Converts a Ruby object (typically a Hash) to JSON, then uploads it to the database.
+  def self.post_document(obj)
+    doc = JSON.generate(obj)
+    uri = uri('')
+    $stdout.puts Net::HTTP.post(uri, doc, JSON_HEADER)
+  end
+
+  ##
+  # Converts a Ruby object (typically a Hash) to JSON, then uploads it to the database as a design document.
+  def self.put_ddoc(name, obj)
+    doc = JSON.generate(obj)
+    uri = uri("_design/#{name}")
+    p uri
+    request = Net::HTTP::Put.new(uri)
+
+    Net::HTTP.start(uri.hostname, uri.port) do |http|
+      $stdout.puts http.request(request, doc)
     end
-        
-    ##
-    # Converts a Ruby object (typically a Hash) to JSON, then uploads it to the database.
-    def self.post_document(obj)
-        doc = JSON.generate(obj)
-        uri = uri(DB)
-        $stdout.puts Net::HTTP.post(uri, doc, JSON_HEADER)
-    end
-
-    ##
-    # Converts a Ruby object (typically a Hash) to JSON, then uploads it to the database as a design document.
-    def self.put_ddoc(name, obj)
-        doc = JSON.generate(obj)
-        uri = uri("#{DB}/_design/#{name}")
-        p uri
-        request = Net::HTTP::Put.new(uri)
-
-        Net::HTTP.start(uri.hostname, uri.port) do |http|
-            $stdout.puts http.request(request, doc)
-        end
-
-    end
+  end
 end
