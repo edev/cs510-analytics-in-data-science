3aeef8d48f1b381c21481944be50f2cb421b9b45
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2020-02-02 17:23:56 -0800

    Added version selection landing page.

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+# Serve the landing page.
 get '/' do
-    'Hell, world! O!'
+  send_file File.expand_path('index.html', settings.public_folder)
 end
